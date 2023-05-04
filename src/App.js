5af2947b5bb860e1c3ec7a3a740b488cb1a7b390
Senan747 <111648854+Senan747@users.noreply.github.com>
2023-05-04 23:03:42 +0400

    second

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
+import Main from './components/Main/Main'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      {/* <Main />
+      <Footer />  */}
+    </>
+  )
+}
+
+export default App
