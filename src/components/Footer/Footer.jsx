7a09524a052a80cef79c884462de3ba683b7ebf2
Senan747 <111648854+Senan747@users.noreply.github.com>
2023-05-07 08:48:07 +0400

    d

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,159 @@
 import React from 'react'
 import './footer.css'
+import './footer.scss'
+import video from '../../assets/video2.mp4'
+import {BsFillSendFill} from 'react-icons/bs'
+import {MdTravelExplore} from 'react-icons/md'
+import {AiOutlineTwitter} from 'react-icons/ai'
+import {AiFillYoutube} from 'react-icons/ai'
+import {AiFillInstagram} from 'react-icons/ai'
+import {FaTripadvisor} from 'react-icons/fa'
+import {FiChevronRight} from 'react-icons/fi'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <section className="footer">
+      <div className="videoDiv">
+        <video src={video} loop autoPlay muted type='video/mp4'></video>
+      </div>
+
+      <div className="setContent container">
+        <div className="contactDiv flex">
+          <div className="text">
+            <small>KEEP IN TOUCH</small>
+            <h2>Travel with us</h2>
+          </div>
+
+          <div className="inputDiv flex">
+            <input type="text" placeholder='Enter email address' />
+            <button className="btn flex" type='submit'>
+              SEND <BsFillSendFill className="icon" />
+            </button>
+          </div>
+        </div>
+
+        <div className="footerCard flex">
+          <div className="footerIntro flex">
+            <div className="logoDiv">
+              <a href="#" className="logo flex">
+                <MdTravelExplore className='icon' /> Travel. 
+              </a>
+            </div>
+
+            <div className="footerParagraph">
+              Traveling is a wonderful way to experience new cultures, meet new 
+              people, and create unforgettable memories. One of the most popular
+              forms of travel is taking a trip. Whether it's a weekend getaway
+              or a month-long adventure, a well-planned trip can be an incredible experience.
+            </div>
+
+            <div className="footerSocials flex">
+              <AiOutlineTwitter className='icon' />
+              <AiFillYoutube className="icon" />
+              <AiFillInstagram className="icon" />
+              <FaTripadvisor className="icon" />
+            </div>
+            
+          </div>
+
+          <div className="footerLinks grid">
+            <div className="linkGroup">
+              <span className="groupTitle">
+                OUR AGENCY
+              </span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Services
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Insurance
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Agency
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Tourism
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Payment
+              </li>
+            </div>
+
+            <div className="linkGroup">
+              <span className="groupTitle">
+                PARTNERS
+              </span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Bookings
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Rentcars
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                HostelWorld
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Trivago
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                TripAdvison
+              </li>
+            </div>
+
+            <div className="linkGroup">
+              <span className="groupTitle">
+                OUR AGENCY
+              </span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Services
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Insurance
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Agency
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Tourism
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Payment
+              </li>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
